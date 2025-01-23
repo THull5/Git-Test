131437b59794c350348f0f9d4013768b4b0f3755
Thull5 <thull5@ut.utm.edu>
2025-01-23 17:56:41 -0600

    Number method added

--- main.cpp
+++ main.cpp
@@ -1,7 +1,24 @@
 #include <iostream>
 using namespace std;
 
+int sum(int n){
+    int sum = 0;
+    for(int i = 1; i <= n; i++){
+        sum += i;
+    }
+    return sum;
+}
+
 int main(){
+  int n;
   cout << "Hello World!" << endl;
+  cout << "Please give a number: ";
+  cin >> n;
+  
+  int total = sum(n);
+  cout << "Your value is " << total << endl;
   return 0;
 }
+
+
+
